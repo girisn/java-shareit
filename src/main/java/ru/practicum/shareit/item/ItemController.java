cf619125d61213a8d84057568db6e14a4d3954f9
Наталия Гирис <n.giris@gt-sot.ru>
2023-08-26 16:55:09 +0300

    ТЗ 15 - fix

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -15,27 +15,23 @@ import static ru.practicum.shareit.util.Constants.REQUEST_HEADER_USER_ID;
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
-@Slf4j
 public class ItemController {
     private final ItemService service;
 
     @PostMapping
     public ItemDto createItem(@Valid @RequestBody ItemShortDto itemShortDto,
                               @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
-        log.info("Item created by user with ID = {} ", userId);
         return service.createItem(itemShortDto, userId);
     }
 
     @GetMapping("/{itemId}")
     public ItemDto getItemById(@PathVariable Long itemId,
                                @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
-        log.info("Get item with ID = {} {} {} ", itemId, " by user with ID = ", userId);
         return service.getItemById(itemId, userId);
     }
 
     @GetMapping
     public List<ItemDto> getAllItems(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
-        log.info("Get all items by user with ID = {}", userId);
         return service.getAllItems(userId);
     }
 
@@ -43,7 +39,6 @@ public class ItemController {
     public ItemDto updateItemById(@RequestBody ItemShortDto itemShortDto,
                                   @PathVariable Long itemId,
                                   @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
-        log.info("Updated item with ID = {} {} {} ", itemId, " by user with ID = ", userId);
         return service.updateItemById(itemShortDto, itemId, userId);
     }
 
@@ -57,7 +52,6 @@ public class ItemController {
     public CommentDto addComment(@PathVariable Long itemId,
                                  @RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
                                  @Valid @RequestBody CommentDto commentDto) {
-        log.info("Comment added for item with ID = {} {} {}", itemId, "by user with ID = ", userId);
         return service.addComment(commentDto, userId, itemId);
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -13,39 +13,33 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/users")
-@Slf4j
 public class UserController {
     private final UserService userService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto createUser(@Validated({Marker.OnCreate.class}) @RequestBody UserDto userDto) {
-        log.info("The user with id = {} has been created", userDto.getId());
         return userService.createUser(userDto);
     }
 
     @GetMapping("/{id}")
     public UserDto getUserById(@PathVariable Long id) {
-        log.info("Get a user by id = {}", id);
         return userService.getUserById(id);
     }
 
     @GetMapping
     public List<UserDto> getAllUsers() {
-        log.info("GET {} users", userService.getAllUsers().size());
         return userService.getAllUsers();
     }
 
     @PatchMapping("/{id}")
     public UserDto updateUserById(@Validated(Marker.OnUpdate.class) @RequestBody UserDto userDto,
                                   @PathVariable Long id) {
-        log.info("The user with id = {} has been updated", id);
         return userService.updateUserById(id, userDto);
     }
 
     @DeleteMapping("/{id}")
     public void removeUserById(@PathVariable Long id) {
-        log.info("The user with id = {} has been removed", id);
         userService.removeUserById(id);
     }
 }
